94a8cedbd837a94297fe2a7d71511bedf81aa03a
Kieran Harrison <17HarriK@fulford.york.sch.uk>
2022-09-26 15:17:25 +0100

    Added recursion for factorial

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -10,6 +10,42 @@ namespace Recursion
     {
         static void Main(string[] args)
         {
+            // 5! means 5 x 4 x 3 x 2 x 1
+
+            // ask the user to enter n (an integer)
+            Console.WriteLine("Welcome to Factorial Factory");
+            Console.Write("Please enter a number: ");
+            int n = 0;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.Write("Please enter a number: ");
+            }
+
+            /*
+            int factorial = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine($"{n}! = {factorial}");
+            */
+
+            int factorial = CalculateFactorial(n);
+
+            Console.WriteLine($"{n}! = {factorial}");
+
+        }
+
+        static int CalculateFactorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * CalculateFactorial(n - 1);
         }
     }
 }
